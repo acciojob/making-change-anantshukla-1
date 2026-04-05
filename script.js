dc41663f9b397fab64bcfc11b27e20a7f7b25217
anantshukla-1 <shukla18anant9839@gmail.com>
2026-04-06 00:01:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const makeChange = (c) => {
   // your name here
+	amount = Number(c);
+	const coins = [
+		{key : q , value : 25},
+		{key : d , value : 10},
+		{key : n , value :  5},
+		{key : p , value :  1}
+	]
+	const result = {q:0, d:0, n:0, p:0};
+
+	for(let coin of coins){
+		result[coins.key] = Math.floor(amount/coins.value);
+		amount = amount % coins.value;
+	}
+	return result;
 };
 
 // Do not the change the code below
